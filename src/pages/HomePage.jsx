5499b6267dfeb8e0878042bc26561a48f8b31be0
helopgom <helena.lopez.gomez@gmail.com>
2024-09-23 12:41:13 +0200

    Added calendar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,20 @@
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+
 const HomePage = () => {
-    return (
-        <div>HomePage</div>
-    )
-}
-export default HomePage
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div>
+      HomePage
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+    </div>
+  );
+};
+
+export default HomePage;
