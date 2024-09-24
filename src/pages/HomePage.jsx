f4443cb13b23dcbc0917acdc0977579bc338e049
jess-ar <jessica18_24@hotmail.com>
2024-09-24 23:51:48 +0200

    PC-68 feat: Add dynamic SessionCard component, SessionCardActions, SessionList. - Applied hover effects to cards, including shadow scaling and primary border color on hover. - Added  functionality to SessionCard, logging session title when clicked (preparing for future redirection). - Responsive design for cards, adjusting layout on different screen sizes. - Currently using fake data for session details, but component is ready for integration with a real API endpoint.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { useToast } from "@/hooks/useToast";
 import { Button } from "@/components/ui/button";
 import RegisterDialog from "@/components/auth/RegisterDialog";
 import HeroSection from "@/components/HeroSection";
+import SessionList from "@/components/session/SessionList";
 
 const HomePage = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -16,15 +17,14 @@ const HomePage = () => {
             description: "Se ha registrado correctamente",
             variant: "destructive",
         })
-
-
     }
 
     return (
         <>
             <HeroSection />
+            <SessionList />
             <section className="flex flex-col items-center justify-center gap-5 text-center">
-                <h3 className="text-xl font-bold">No te lo pienses mas</h3>
+                <h3 className="text-xl font-bold">No te lo pienses más</h3>
                 <Button variant="specialShadow" onClick={() => setIsOpen(true)} size="lg">Regístrate</Button>
             </section>
             <RegisterDialog open={isOpen} onOpenChange={setIsOpen} handleSubmit={handleSubmit} />
