be19a8aca12f37c7c938ba142f65052f7aca1354
Valentina Toni <vale.toni93@gmail.com>
2024-09-25 21:50:08 +0200

    DONE: corrected register ha ndle error

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -73,6 +73,8 @@ const LoginPage = () => {
                     errorMessage = errorData.password.join(" ");
                 } else if (errorData.email) {
                     errorMessage = errorData.email.join(" ");
+                } else if (errorData.username) {
+                    errorMessage = errorData.username.join(" ");
                 } else if (errorData.non_field_errors) {
                     errorMessage = errorData.non_field_errors.join(" ");
                 }
