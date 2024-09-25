281dbe0231fa9abbebcd3c03b7d25e03a06c4d57
helopgom <helena.lopez.gomez@gmail.com>
2024-09-25 16:20:42 +0200

    Changed styles. Missing if authenticated logout, to see the project

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,82 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import ModeToggle from './ModeToggle';
-import { NavLink } from 'react-router-dom';
-import { Menu } from 'lucide-react';
-import { Button } from '../ui/button';
-import { useAuth } from '@/hooks/useAuth'; 
+import React, { useState, useEffect } from "react";
+import ModeToggle from "./ModeToggle";
+import { NavLink } from "react-router-dom";
+import { Menu } from "lucide-react";
+import { Button } from "../ui/button";
+import { useAuth } from "@/hooks/useAuth";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const { isAuthenticated } = useAuth();
-    const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const { isAuthenticated } = useAuth();
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [menuColor, setMenuColor] = useState("");
 
-    const navlinks = [
-        { name: "Home", to: "/" },
-        { name: "Sobre nosotros", to: "/about-us" },
-        { name: "Pair programming", to: "/pair-programming" },
-    ];
+  const navlinks = [
+    { name: "Home", to: "/" },
+    { name: "Sobre nosotros", to: "/about-us" },
+    { name: "Pair programming", to: "/pair-programming" },
+  ];
 
-    const toggleMenu = () => setIsOpen(!isOpen);
+  const toggleMenu = () => setIsOpen(!isOpen);
 
-    // Detectar si el modo oscuro está activado
-    useEffect(() => {
-        const isDark = document.documentElement.classList.contains('dark');
-        setIsDarkMode(isDark);
-    }, []);
+  return (
+    <nav className={`shadow-lg px-6 py-4`}>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center space-x-3">
+          <NavLink to="/" className="h-10">
+            <img src="/logo.svg" alt="logo" className="w-[35px]" />
+          </NavLink>
+          <span
+            className="font-poppins font-bold text-[36px] leading-[120%] hidden md:block text-transparent bg-clip-text"
+            style={{ backgroundImage: "var(--gradient)" }}
+          >
+            Pair Connect
+          </span>
+        </div>
 
-    return (
-        <nav className={`shadow-lg px-6 py-4`}>
-            <div className="flex justify-between items-center">
-                <div className="flex items-center space-x-3">
-                    <div className=" h-10">
-                        <img src='/logo.svg' alt='logo' className='w-[40px]'></img>
-                    </div>
-                    <span className="font-poppins font-bold text-[36px] leading-[120%] hidden md:block text-transparent bg-clip-text" style={{ backgroundImage: 'var(--gradient)' }}>Pair Connect</span>
-                </div>
+        {/* Links de navegación para pantallas grandes */}
+        <div className="hidden lg:flex space-x-8">
+          {navlinks.map((item) => (
+            <NavLink key={item.name} to={item.to}>
+              {item.name}
+            </NavLink>
+          ))}
+          {isAuthenticated && <NavLink to="/profile">Mi perfil</NavLink>}
+        </div>
 
-                {/* Links para escritorio */}
-                <div className="hidden lg:flex space-x-8">
-                    {navlinks.map((item) => (
-                        <NavLink key={item.name} to={item.to} className="">{item.name}</NavLink>
-                    ))}
-                    {/* Mostrar "Mi perfil" si el usuario está autenticado */}
-                    {isAuthenticated && (
-                        <NavLink to="/profile" className="">Mi perfil</NavLink>
-                    )}
-                </div>
+        {/* Toggle, Login y Menú */}
+        <div className="flex items-center space-x-4">
+          <ModeToggle />
+          <Button>Login</Button>
+          <div className="lg:hidden">
+            {" "}
+            {/* Menú móvil visible solo en pantallas pequeñas */}
+            <button onClick={toggleMenu} className="focus:outline-none">
+              <Menu size={40} />
+            </button>
+          </div>
+        </div>
+      </div>
 
-                <div className="flex items-center space-x-4">
-                    {/* Botón para alternar el tema */}
-                    <ModeToggle />
-                    {/* Botón de Login */}
-                    <Button>login</Button>
-                </div>
-
-                {/* Botón de menú móvil */}
-                <div className="md:hidden">
-                    <button 
-                        onClick={toggleMenu} 
-                        className={`${isDarkMode ? 'text-white' : 'text-black'} focus:outline-none`}
-                    >
-                        <Menu />
-                    </button>
-                </div>
-            </div >
-
-            {/* Menú móvil */}
-            {isOpen && (
-                <div className="md:hidden flex flex-col items-left space-y-2 py-5">
-                    {navlinks.map((item) => (
-                        <NavLink key={item.name} to={item.to} className="">{item.name}</NavLink>
-                    ))}
-                    {/* Mostrar "Mi perfil" si el usuario está autenticado en el menú móvil */}
-                    {isAuthenticated && (
-                        <NavLink to="/profile" className="">Mi perfil</NavLink>
-                    )}
-                </div>
-            )}
-        </nav>
-    );
+      {/* Menú móvil para pantallas pequeñas */}
+      {isOpen && (
+        <div className="md:hidden flex flex-col items-left space-y-2 py-5">
+          {navlinks.map((item) => (
+            <NavLink key={item.name} to={item.to}>
+              {item.name}
+            </NavLink>
+          ))}
+          {isAuthenticated && <NavLink to="/profile">Mi perfil</NavLink>}
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
