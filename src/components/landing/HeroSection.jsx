9cd0ca8007532079f0cb261e951d4308aa1de52c
jess-ar <jessica18_24@hotmail.com>
2024-09-26 13:14:36 +0200

    MD-110 feat: update HeroSection layout and styling to responsive with new sizes

--- src/components/landing/HeroSection.jsx
+++ src/components/landing/HeroSection.jsx
@@ -1,16 +1,17 @@
 /* eslint-disable react/prop-types */
 import HeroButton from "./HeroButton";
+import 'animate.css';
 
 const HeroSection = ({handleRegisterClick}) => {
     return (
-        <section className="flex flex-col items-center justify-center pt-2 mt-16 text-center bg-background"> 
-            <h1 className="mb-4 font-poppins font-bold text-6xl leading-[120%] text-transparent bg-clip-text" style={{ backgroundImage: 'var(--gradient)' }}>
+        <section className="flex flex-col items-center justify-center pt-2 mt-40 mb-40 text-center bg-background"> 
+            <h1 className="mb-10 font-poppins font-bold text-6xl md:text-7xl lg:text-9xl leading-[120%] text-transparent bg-clip-text" style={{ backgroundImage: 'var(--gradient)' }}>
                 Pair Connect
             </h1>
-            <p className="mb-6 text-lg text-foreground">
-                Encuentra tu colega para aprender y codificar.
+            <p className="text-xl mb-28 md:text-2xl lg:text-3xl text-foreground">
+                Conecta, programa y crece en equipo
             </p>
-            <HeroButton text="Registrarme" onClick={handleRegisterClick} />
+            <HeroButton text="Regístrate aquí" onClick={handleRegisterClick} />
         </section>
     );
 };
