3f299d3b69d74efbf3d55a8463dbc06b53bb9c26
jess-ar <jessica18_24@hotmail.com>
2024-10-01 11:41:03 +0200

    PC-135 feat: added gradient2 variable and refactored neon-border class
    - Added gradient2 variable for consistent gradient usage
    - Combined and refactored the neon-border class for better clarity and efficiency

--- src/components/about/AboutUsContent.jsx
+++ src/components/about/AboutUsContent.jsx
@@ -2,9 +2,9 @@ const AboutUsContent = () => {
     return (
         <div className="relative flex flex-col items-center justify-center p-4 m-0 mt-10">
             <div className="max-w-4xl cursor-pointer neon-border bg-background">
-                <h4 className="mb-8 text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-[#f05cba] via-[#18e0ea] to-[#ef40b5] dark:from-[#db3eb4] dark:via-[#eed5e0] dark:to-[#db3eb4]">
+                <h1 className="mb-8 text-4xl font-bold text-center text-transparent bg-clip-text gradient2-text">
                     Sobre el equipo
-                </h4>
+                </h1>
                 <p className="mb-4 text-base text-center text-foreground">
                     Somos seis chicas llenas de energía que, tras enfrentarnos al intenso bootcamp de Factoría F5 en el programa FemCoders Promoción5 (Barcelona y Norte), hemos unido nuestras fuerzas para crear este proyecto grupal pedagógico final.
                 </p>
--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,4 +1,3 @@
-import '@/styles/about-us-page.css';
 import '@/styles/cosmic-background.css';
 import CosmicBackground from '@/components/shared/CosmicBackground';
 import AboutUsContent from '@/components/about/AboutUsContent';
