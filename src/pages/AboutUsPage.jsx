af53315960075f3a3ecd95f5e18ed876e2bb1104
jess-ar <jessica18_24@hotmail.com>
2024-10-02 18:49:00 +0200

    PC-136 feat: Implemented reusable TeamCard component. Added shadow-white to variables. Pending centering last card

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -5,27 +5,39 @@ import { Button } from '@/components/ui/button';
 import { useRegister } from '@/hooks/useRegister';
 import RegisterDialog from "@/components/auth/RegisterDialog";
 import { useState, useRef } from "react";
+import TeamList from '@/components/team/TeamList';
 
 const AboutUsPage = () => {
     const [isOpen, setIsOpen] = useState(false);
     const { handleRegister, loading } = useRegister();
 
     const registerButtonRef = useRef(null);
+    const teamSectionRef = useRef(null); // Ref para la sección del equipo
 
-    const scrollToRegisterButton = () => {
-        if (registerButtonRef.current) {
-            registerButtonRef.current.scrollIntoView({ behavior: "smooth" });
+    // Función para desplazarse hacia la sección del equipo
+    const scrollToTeamSection = () => {
+        if (teamSectionRef.current) {
+            teamSectionRef.current.scrollIntoView({ behavior: "smooth" });
         }
     };
 
     return (
-        <section className="relative min-h-screen" >
+        <section className="relative min-h-screen">
             <CosmicBackground />
 
-            <AboutUsContent onClick={scrollToRegisterButton} />
+            {/* Este onClick ahora desplaza hacia la sección del equipo */}
+            <AboutUsContent onClick={scrollToTeamSection} />
 
             <div className="flex flex-col items-center justify-center gap-5 mt-20 mb-20 text-center">
+
+                {/* Ref para la sección del equipo */}
+                <div ref={teamSectionRef}>
+                    <TeamList />
+                </div>
+
                 <h3 className="mb-6 text-xl font-bold">¡Únete a Pair Connect!</h3>
+
+                {/* Botón de registro */}
                 <Button
                     variant="doubleColorButton"
                     onClick={() => setIsOpen(true)}
@@ -37,6 +49,7 @@ const AboutUsPage = () => {
                 </Button>
             </div>
 
+            {/* Modal de registro */}
             <RegisterDialog
                 open={isOpen}
                 onOpenChange={setIsOpen}
