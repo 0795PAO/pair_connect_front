3e409e5ee6515dcb080530e7bdd6268f1e05e8b0
Valentina Toni <vale.toni93@gmail.com>
2024-10-04 18:10:49 +0200

    changed default image

--- src/components/session/SessionList.jsx
+++ src/components/session/SessionList.jsx
@@ -1,19 +1,14 @@
 /* eslint-disable react/prop-types */
 import SessionCard from '@/components/session/SessionCard';
 import Loader from '../shared/Loader';
-
-const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const options = { day: 'numeric', month: 'long', year: 'numeric' };
-    return date.toLocaleDateString('es-ES', options);
-};
+import { formatDate } from '@/utils/formaDateAndTime';
 
 const groupSessionsByDate = (sessions) => {
     if (!sessions || sessions.length === 0) {
         return {};
     }
     return sessions.reduce((grouped, session) => {
-        const date = formatDate(session.date);
+        const date = formatDate(session.schedule_date_time);
         if (!grouped[date]) {
             grouped[date] = [];
         }
@@ -22,9 +17,33 @@ const groupSessionsByDate = (sessions) => {
     }, {});
 };
 
+const getFourDayRange = (startDate) => {
+    const start = new Date(startDate);
+    const end = new Date(start);
+    end.setDate(start.getDate() + 4);
+    return { start, end };
+};
 
-const SessionList = ({sessions, loading, error}) => {
-    
+const filterSessionsByFourDays = (sessions) => {
+    if (!sessions || sessions.length === 0) {
+        return [];
+    }
+    const sortedSessions = sessions.sort((a, b) => new Date(a.schedule_date_time) - new Date(b.schedule_date_time));
+
+    const firstSessionDate = sortedSessions[0].schedule_date_time;
+
+    const { start, end } = getFourDayRange(firstSessionDate);
+
+    return sortedSessions.filter(session => {
+        const sessionDate = new Date(session.schedule_date_time);
+        return sessionDate >= start && sessionDate <= end;
+    });
+};
+
+
+
+const SessionList = ({ sessions, loading, error }) => {
+
 
     if (loading) {
         return <Loader />;
@@ -34,23 +53,24 @@ const SessionList = ({sessions, loading, error}) => {
         return <p>Error al cargar las sesiones.</p>;
     }
     console.log(sessions)
-    const sessionsByDate = groupSessionsByDate(sessions);
+    const filteredSessions = filterSessionsByFourDays(sessions);
+    const sessionsByDate = groupSessionsByDate(filteredSessions);
     return (
         <section className="max-w-6xl mx-auto my-10">
             <h3 className="mb-4 text-4xl font-bold">Sesiones Programadas:</h3>
-
-            {sessions && sessions.length > 0 && Object.keys(sessionsByDate).map((date) => (
-                <div key={date} className="mb-8">
-                    <h4 className="mb-4 text-lg font-semibold">{date}</h4>
-                    <div className="grid justify-center grid-cols-1 gap-6 md:grid-cols-2">
-                        {sessionsByDate[date].map((session, index) => (
-                            <div key={index} className="mx-auto">
-                                <SessionCard session={session} />
-                            </div>
-                        ))}
+                {sessions && sessions.length > 0 && Object.keys(sessionsByDate).map((date) => (
+                    <div key={date} >
+                        <h4 className="mb-4 text-lg font-semibold">{date}</h4>
+                        <ul className={`grid gap-6 items-stretch ${sessionsByDate[date].length === 1
+                                ? "grid-cols-1" 
+                                : "grid-cols-1 md:grid-cols-2" 
+                            }`}>
+                            {sessionsByDate[date].map((session, index) => (
+                                <SessionCard session={session} key={index} />
+                            ))}
+                        </ul>
                     </div>
-                </div>
-            ))}
+                ))}
         </section>
     );
 };
