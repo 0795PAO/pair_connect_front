8df87e762d5501b6f97742dc3693b0b42be3e7ff
helopgom <helena.lopez.gomez@gmail.com>
2024-10-07 12:53:35 +0200

    Added all sections filters

--- src/pages/UserHomePage.jsx
+++ src/pages/UserHomePage.jsx
@@ -1,57 +1,75 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { EventCalendar } from "@/components/shared/EventCalendar";
 import Loader from "@/components/shared/Loader";
 import { useProfile } from "@/hooks/useProfile";
 import { useSuggestedSessions } from "@/hooks/useSuggestedSessions";
 import CompleteProfileModal from "@/components/profile/CompleteProfileModal";
 import SessionCard from "@/components/session/SessionCard";
+import SessionFilter from "@/components/session/SessionFilter";
+import { useSessionFilter } from "@/hooks/useSessionFilter";
+import { Button } from "@/components/ui/button";
 
 const UserHomePage = () => {
   const { data: user, isLoading: isProfileLoading, error } = useProfile();
   const [open, setOpen] = useState(false);
-
   const { data: sessions = [], isLoading: isSessionsLoading } =
     useSuggestedSessions();
+  const sessionListRef = useRef(null);
+  const sessionFilterRef = useRef(null); // Ref para SessionFilter
+
+  const {
+    filteredSessions,
+    searchTerm,
+    setSearchTerm,
+    selectedStack,
+    setSelectedStack,
+    selectedLevel,
+    setSelectedLevel,
+    selectedDate,
+    setSelectedDate,
+    applyFilters,
+  } = useSessionFilter(sessions);
 
   useEffect(() => {
     if (user && (!user.prog_language || !user.stack)) {
       setOpen(true);
     }
   }, [user]);
-  console.log("Sessions in UserHomePage:", sessions);
+
+  const handleSearchSessions = () => {
+    applyFilters();
+  };
 
   return (
-    <div
-      data-testid="user-home-page"
-      className="flex flex-col items-center w-full gap-5"
-    >
+    <div className="user-home-page flex flex-col items-center w-full gap-5">
       {isProfileLoading && <Loader />}
       {error && <p>Error: {error.message}</p>}
       {user && (
         <>
-          <h1
-            className="mb-4 font-poppins font-bold text-6xl leading-[120%] text-transparent bg-clip-text"
-            style={{ backgroundImage: "var(--gradient)" }}
+          <h1>Pair Connect</h1>
+          <h2>Hola, {user.username}</h2>
+
+          <div className="flex flex-col md:flex-row w-full">
+            <SessionFilter
+              ref={sessionFilterRef} // Pasar ref
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
+              selectedStack={selectedStack}
+              setSelectedStack={setSelectedStack}
+              selectedLevel={selectedLevel}
+              setSelectedLevel={setSelectedLevel}
+            />
+            <EventCalendar />
+          </div>
+
+          <Button
+            variant="outline"
+            onClick={handleNext}
+            disabled={isLastSession(sessions, startDate)}
           >
-            Pair Connect
-          </h1>
-          <h2 className="self-start text-3xl font-semibold">
-            Hola, {user?.username}
-          </h2>
+            <CircleChevronRight />
+          </Button>
 
-          {isSessionsLoading ? (
-            <Loader />
-          ) : sessions.length > 0 ? (
-            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {sessions.map((session) => (
-                <SessionCard key={session.id} session={session} />
-              ))}
-            </ul>
-          ) : (
-            <p>No hay sesiones sugeridas disponibles.</p>
-          )}
-
-          <EventCalendar />
           <CompleteProfileModal open={open} onOpenChange={setOpen} />
         </>
       )}
