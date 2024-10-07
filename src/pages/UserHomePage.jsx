9101ff9cec29a1b9167fdddf3108eb711418813c
helopgom <helena.lopez.gomez@gmail.com>
2024-10-07 14:07:54 +0200

    Added other sessions

--- src/pages/UserHomePage.jsx
+++ src/pages/UserHomePage.jsx
@@ -14,8 +14,7 @@ const UserHomePage = () => {
   const [open, setOpen] = useState(false);
   const { data: sessions = [], isLoading: isSessionsLoading } =
     useSuggestedSessions();
-  const sessionListRef = useRef(null);
-  const sessionFilterRef = useRef(null); // Ref para SessionFilter
+  const sessionListRef = useRef(null); // Ref para sesiones filtradas
 
   const {
     filteredSessions,
@@ -40,18 +39,43 @@ const UserHomePage = () => {
     applyFilters();
   };
 
+  const disableNextButton = filteredSessions.length === 0;
+
   return (
     <div className="user-home-page flex flex-col items-center w-full gap-5">
       {isProfileLoading && <Loader />}
       {error && <p>Error: {error.message}</p>}
       {user && (
         <>
-          <h1>Pair Connect</h1>
-          <h2>Hola, {user.username}</h2>
+          <h1
+            className="mb-4 font-poppins font-bold text-6xl leading-[120%] text-transparent bg-clip-text"
+            style={{ backgroundImage: "var(--gradient)" }}
+          >
+            Pair Connect
+          </h1>
+          <h2 className="self-start text-3xl font-semibold">
+            Hola, {user?.username}
+          </h2>
+          <h3 className="self-start text-xl ">Sesiones sugeridas para ti:</h3>
 
-          <div className="flex flex-col md:flex-row w-full">
+          <div ref={sessionListRef} className="mt-5">
+            {isSessionsLoading ? (
+              <Loader />
+            ) : filteredSessions.length > 0 ? (
+              <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {filteredSessions.map((session) => (
+                  <SessionCard key={session.id} session={session} />
+                ))}
+              </ul>
+            ) : (
+              <p>No hay sesiones sugeridas disponibles.</p>
+            )}
+            <h3 className="self-start text-xl mt-16 ">
+              ¡Busca otras sesiones de tu interés!
+            </h3>
+          </div>
+          <div className="flex flex-col md:flex-row w-full gap-4">
             <SessionFilter
-              ref={sessionFilterRef} // Pasar ref
               searchTerm={searchTerm}
               setSearchTerm={setSearchTerm}
               selectedStack={selectedStack}
@@ -59,15 +83,14 @@ const UserHomePage = () => {
               selectedLevel={selectedLevel}
               setSelectedLevel={setSelectedLevel}
             />
-            <EventCalendar />
+            <EventCalendar
+              selectedDate={selectedDate}
+              setSelectedDate={setSelectedDate}
+            />
           </div>
 
-          <Button
-            variant="outline"
-            onClick={handleNext}
-            disabled={isLastSession(sessions, startDate)}
-          >
-            <CircleChevronRight />
+          <Button variant="specialShadow" onClick={handleSearchSessions}>
+            Buscar sesiones
           </Button>
 
           <CompleteProfileModal open={open} onOpenChange={setOpen} />
